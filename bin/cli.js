b0934784af792ce0dd4d057becabb1018a610fc3
VS <valeriesho@unc.edu>
2022-11-30 17:18:11 -0500

    first pass through of cli

--- bin/cli.js
+++ bin/cli.js
@@ -3,3 +3,18 @@
 import minimist from "minimist";
 import { roll } from "/lib/roll.js";
 
+const args = minimist(process.argv.slice(2));
+
+var sides = 6;
+var dice = 2;
+var rolls = 1;
+if (args.sides) {
+    sides = args.sides;
+}
+if (args.dice) {
+    dice = args.dice;
+}
+if (args.rolls) {
+    rolls = args.rolls;
+}
+
